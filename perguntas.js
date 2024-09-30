29ee940e60e4d7e0d9bde63b66b5121c639bc1e2
leal <leal.souza.maria@escola.pr.gov.br>
2024-09-30 10:57:46 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'programacao',
+    'o que e python',
+    'o python e uma linguagem'
+)
+
+criaCartao(
+    'geografia',
+    'qual a cap da franca', 
+    'a cap e paris',
+)
+
+criaCartao(
+    'programacao',
+    'oq e uma funcao',
+    'uma funcao e'
+)
+
+criaCartao(
+    'lingua inglesa',
+    'comos e diz oi',
+    'resposta hi'
+)
